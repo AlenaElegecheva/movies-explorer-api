c4cbcc76053b527f4103652ea068cc3cd3f0ded7
Alena Elegecheva <alena.elegecheva@gmail.com>
2023-07-02 00:09:30 +0700

    Исправить количество запросов в rateLimit

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,5 +2,5 @@ const rateLimit = require('express-rate-limit');
 
 module.exports = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 100, // можно совершить максимум 100 запросов с одного IP
+  max: 500, // можно совершить максимум 500 запросов с одного IP
 });
